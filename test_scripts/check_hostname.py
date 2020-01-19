6eb23f024856baf036900e2db50618c3d780a78e
sdwill <sdwill94@gmail.com>
2020-01-19 10:26:27 -0500

    Use prints instead of logs
    The print output appears to be saved to the log file, NOT the log...

--- test_scripts/check_hostname.py
+++ test_scripts/check_hostname.py
@@ -1,7 +1,5 @@
 import socket
-import logging
 
-log = logging.getLogger(__name__)
-log.info('Hostname:', socket.gethostname())
-log.info('First three letters:', socket.gethostname()[:3])
-log.info('Is this the same as bhc?', socket.gethostname()[:3] == 'bhc')
+print('Hostname:', socket.gethostname())
+print('First three letters:', socket.gethostname()[:3])
+print('Is this the same as bhc?', socket.gethostname()[:3] == 'bhc')
