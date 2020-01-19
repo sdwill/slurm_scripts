72ac0eeeba4dbd755f5b65d39dcb7b720dc7b9ad
sdwill <sdwill94@gmail.com>
2020-01-19 10:24:50 -0500

    Fix bug in previous commit

--- test_scripts/check_hostname.py
+++ test_scripts/check_hostname.py
@@ -4,4 +4,4 @@ import logging
 log = logging.getLogger(__name__)
 log.info('Hostname:', socket.gethostname())
 log.info('First three letters:', socket.gethostname()[:3])
-log.info('Is this the same as bhc?', socket.gethostname()[:3] == bhc)
+log.info('Is this the same as bhc?', socket.gethostname()[:3] == 'bhc')
