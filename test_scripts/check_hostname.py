6dd726a7c0a35b9940e31eaff0bf35ea60976a13
sdwill <sdwill94@gmail.com>
2020-01-19 10:23:13 -0500

    Change output because log message didn't print

--- test_scripts/check_hostname.py
+++ test_scripts/check_hostname.py
@@ -2,6 +2,6 @@ import socket
 import logging
 
 log = logging.getLogger(__name__)
-log.info(socket.gethostname())
-log.info(socket.gethostname()[:3] == 'bhc')
-print(socket.gethostname())
+log.info('Hostname:', socket.gethostname())
+log.info('First three letters:', socket.gethostname()[:3])
+log.info('Is this the same as bhc?', socket.gethostname()[:3] == bhc)
